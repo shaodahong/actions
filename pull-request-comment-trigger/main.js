db5e47cf0b8da871bfa139feb3008bd0abdb49c0
shaodahong <darryshaw@gmail.com>
2020-05-13 15:23:29 +0800

    test

--- pull-request-comment-trigger/main.js
+++ pull-request-comment-trigger/main.js
@@ -13,7 +13,7 @@ async function run() {
 
   const body =
     context.eventName === 'issue_comment'
-      ? context.payload.comment.body
+      ? context.payload.issue.body
       : context.payload.pull_request.body;
   core.setOutput('comment_body', body);
 
